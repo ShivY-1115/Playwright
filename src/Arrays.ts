46ef827a43cb3350d21bb98128c2c2b487d8385a
SriKaR_323 <srikar1115@gmail.com>
2025-10-14 17:44:32 +0530

    Update Arrays.ts

--- src/Arrays.ts
+++ src/Arrays.ts
@@ -1,17 +1,17 @@
-let values2: string[] = ["Vamsi","Srikar","Jogi"];
+let values2: string[] = ["Stark","Jon","Snow"];
 
 console.log(values2);
 
-let array:Array<Object> = [123,"Vamsi","Srikar","Jogi"];
-let array1:Array<any> = [123,"Vamsi","Srikar","Jogi"];
+let array:Array<Object> = [123,"Stark","Jon","Snow"];
+let array1:Array<any> = [123,"Stark","Jon","Snow"];
 
 
 console.log(array)
 
 console.log(array);
 
-let names:(string | number)[] = ['Vamsi','Srikar','Jogi',23];
-let array2:Array<string | number> = ['Vamsi','Srikar','Jogi',23,47];
+let names:(string | number)[] = ["Stark","Jon","Snow",23];
+let array2:Array<string | number> = ["Stark","Jon","Snow",23,47];
 array2.push('Vamsi');
 for(let i=0;i<array2.length;i++){
     console.log(array2[i]);
