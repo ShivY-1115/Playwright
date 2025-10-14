66566cf5672246a70b9a240b13a39883523983d5
SriKaR_323 <srikar1115@gmail.com>
2025-10-14 17:42:40 +0530

    Update Tuple.ts

--- src/Tuple.ts
+++ src/Tuple.ts
@@ -1,10 +1,10 @@
-let person:[string,number] = ["Vamsi",12];
+let person:[string,number] = ["Aegon",12];
 console.log(person[1]);
 
-let persons:[string,number] []  = [["Naveen",123],["Srikar",23]];
+let persons:[string,number] []  = [["Jon",123],["Snow",23]];
 console.log(persons[1]);
 console.log(persons[1][0]);
 
-persons.push(["Vamsi",323]);
+persons.push(["Aegon",323]);
 console.log(persons);
 
